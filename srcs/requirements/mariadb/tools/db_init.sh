b6d07d672835b098a86e11525861fc92e09b0b83
rarnold <rarnold@student.42.fr>
2024-03-09 14:22:54 +0100

    Change sql syntax in script

--- srcs/requirements/mariadb/tools/db_init.sh
+++ srcs/requirements/mariadb/tools/db_init.sh
@@ -11,7 +11,7 @@ else
 	echo "CREATE DATABASE $SQL_DATABASE;" > init.sql
 	echo "CREATE USER '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';" >> init.sql
 	echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%';" >> init.sql
-	echo "ALTER USER root@localhost IDENTIFIED VIA mysql_native_password USING PASSWORD("$SQL_ROOT_PASSWORD");" >> init.sql
+	echo "ALTER USER root@localhost IDENTIFIED VIA mysql_native_password USING PASSWORD('$SQL_ROOT_PASSWORD');" >> init.sql
 	echo "FLUSH PRIVILEGES;" >> init.sql
 	mysql < init.sql && rm init.sql
 	echo "Database is initialized."
