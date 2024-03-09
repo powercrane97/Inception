af6fc53ec864f04f0c4c712f64faa17876153070
rarnold <rarnold@student.42.fr>
2024-03-09 14:05:20 +0100

    Change mariadb root auth and healthcheck

--- srcs/requirements/mariadb/tools/db_init.sh
+++ srcs/requirements/mariadb/tools/db_init.sh
@@ -11,14 +11,14 @@ else
 	echo "CREATE DATABASE $SQL_DATABASE;" > init.sql
 	echo "CREATE USER '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';" >> init.sql
 	echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%';" >> init.sql
+	echo "ALTER USER root@localhost IDENTIFIED VIA mysql_native_password USING PASSWORD("$SQL_ROOT_PASSWORD");" >> init.sql
 	echo "FLUSH PRIVILEGES;" >> init.sql
 	mysql < init.sql && rm init.sql
-	mysqladmin -u root password $SQL_ROOT_PASSWORD
 	echo "Database is initialized."
 fi
 	
 # shutting down mysql service
 mysqladmin -uroot --password=$SQL_ROOT_PASSWORD shutdown
 	
-# launching mysql in foreground with pid=1
+# launching mysqld in foreground with pid=1
 exec mysqld
